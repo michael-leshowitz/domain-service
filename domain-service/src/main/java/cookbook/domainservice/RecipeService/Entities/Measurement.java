5c33fbdceb5a505464327a0777c11ff2576b26f8
Michael <michael.leshowitz@gmail.com>
2023-03-30 02:26:58 +0000

    Full rest PoC implmented

--- domain-service/src/main/java/cookbook/domainservice/RecipeService/Entities/Measurement.java
+++ domain-service/src/main/java/cookbook/domainservice/RecipeService/Entities/Measurement.java
@@ -1,15 +1,16 @@
 package cookbook.domainservice.RecipeService.Entities;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Entity
 @Data
+@Table(name = "Measurement")
 public class Measurement {
     @Id
     @Column(name="idMeasurement")
--- domain-service/src/main/java/cookbook/domainservice/RecipeService/Repositories/MeasurementRepository.java
+++ domain-service/src/main/java/cookbook/domainservice/RecipeService/Repositories/MeasurementRepository.java
@@ -1,9 +1,11 @@
 package cookbook.domainservice.RecipeService.Repositories;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import cookbook.domainservice.RecipeService.Entities.Measurement;
 
+@Repository
 public interface MeasurementRepository extends CrudRepository<Measurement, Integer>{
     
 }
