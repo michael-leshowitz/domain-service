5c854a007b69a46da572de0b384472d2ca78f5b8
Michael <michael.leshowitz@gmail.com>
2023-04-03 03:32:38 +0000

    added recipe and active ingredient entities

---
+++ domain-service/src/main/java/cookbook/domainservice/recipe_service/service_impl/entities/ActiveRecipeIngredient.java
@@ -0,0 +1,35 @@
+package cookbook.domainservice.recipe_service.service_impl.entities;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "ActiveRecipeIngredient")
+public class ActiveRecipeIngredient implements Serializable {
+    // Note: No repository needed for this, Active recipe ingredients don't have value on own
+
+    private Integer step;
+
+    @Id
+    @ManyToOne
+    @JoinColumn(name="idRecipe", nullable = false)
+    private Recipe recipe;
+
+    @Id
+    @OneToOne
+    private ActiveIngredient ingredient;
+
+    @OneToOne
+    private CookingAction action;
+}
