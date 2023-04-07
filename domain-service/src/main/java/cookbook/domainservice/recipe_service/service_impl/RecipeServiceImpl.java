883e7c643a9fbee25e0789f116a9fde31fa81633
Michael <michael.leshowitz@gmail.com>
2023-04-07 02:52:57 +0000

    Added mapping for lists

--- domain-service/src/main/java/cookbook/domainservice/recipe_service/service_impl/RecipeServiceImpl.java
+++ domain-service/src/main/java/cookbook/domainservice/recipe_service/service_impl/RecipeServiceImpl.java
@@ -5,21 +5,20 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 import cookbook.domainservice.recipe_service.api.RecipeService;
 import cookbook.domainservice.recipe_service.api.models.Recipe;
+import cookbook.domainservice.recipe_service.service_impl.mappers.RecipeMapper;
 import cookbook.domainservice.recipe_service.service_impl.repositories.RecipeRepository;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
-import lombok.Builder;
 
 @Service
-@Builder
 @RequiredArgsConstructor
 public class RecipeServiceImpl implements RecipeService {
 
     @NonNull private final RecipeRepository recipeRepository;
+    @NonNull private final RecipeMapper recipeMapper;
 
     public List<Recipe> searchRecipesByKeyword(String keyword) {
-        List<Recipe> placeholder = new ArrayList<Recipe>();
-        return placeholder;
+        return recipeMapper.entityToModel(recipeRepository.fullTextByKeyword(keyword));
     }
     // Mapping from entity to model
     // RecipeMapper.Instance.entityToModle(...)
--- domain-service/src/main/java/cookbook/domainservice/recipe_service/service_impl/mappers/RecipeMapper.java
+++ domain-service/src/main/java/cookbook/domainservice/recipe_service/service_impl/mappers/RecipeMapper.java
@@ -1,5 +1,7 @@
 package cookbook.domainservice.recipe_service.service_impl.mappers;
 
+import java.util.List;
+
 import org.mapstruct.Mapper;
 
 import cookbook.domainservice.recipe_service.api.models.Recipe;
@@ -9,6 +11,8 @@ import cookbook.domainservice.recipe_service.service_impl.entities.RecipeEntity;
 public interface RecipeMapper {
     
     Recipe entityToModel(RecipeEntity recipeEntity);
+    List<Recipe> entityToModel(List<RecipeEntity> recipeEntity);
 
     RecipeEntity modelToEntity(Recipe recipe);
+    List<RecipeEntity> modelToEntity(List<Recipe> recipe);
 }
